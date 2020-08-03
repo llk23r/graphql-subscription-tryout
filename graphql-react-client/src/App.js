9b76f21e286190a1488a4095ef8c0226207d2313
llk23r <mail.acharyarahul.now@gmail.com>
2020-08-03 11:38:43 +0530

    feat(baseline): Create the React app and the Rails app

---
+++ graphql-react-client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Links from './Links'
+
+class App extends Component {
+  render() {
+    return <Links />
+  }
+}
+
+export default App;
